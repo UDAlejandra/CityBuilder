9864b54fbc5dcee4c97205b82c4125a8ec27f36b
Ivanlopez-24 <ivanlopez24.88@gmail.com>
2026-05-31 18:11:48 -0500

    Implement binary search tree for Building objects

---
+++ src/engine/tree.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Building {
+
+    string name;
+    int happiness;
+};
+
+struct TreeNode {
+
+    Building data;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+class BST {
+
+private:
+
+    TreeNode* root;
+
+    TreeNode* insert(TreeNode* node, Building b) {
+
+        if(node == nullptr) {
+
+            TreeNode* newNode = new TreeNode;
+
+            newNode->data = b;
+            newNode->left = nullptr;
+            newNode->right = nullptr;
+
+            return newNode;
+        }
+
+        if(b.happiness < node->data.happiness)
+            node->left = insert(node->left, b);
+        else
+            node->right = insert(node->right, b);
+
+        return node;
+    }
+
+    void inorder(TreeNode* node) {
+
+        if(node == nullptr)
+            return;
+
+        inorder(node->left);
+
+        cout
+            << node->data.name
+            << " "
+            << node->data.happiness
+            << endl;
+
+        inorder(node->right);
+    }
+
+public:
+
+    BST() {
+        root = nullptr;
+    }
+
+    void insert(Building b) {
+        root = insert(root, b);
+    }
+
+    void print() {
+        inorder(root);
+    }
+};
