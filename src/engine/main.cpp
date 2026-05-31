5dff40c2de6d4dbc816e419145ac14d77d49129e
Ivanlopez-24 <ivanlopez24.88@gmail.com>
2026-05-31 18:31:15 -0500

    Refactor main.cpp to read and process building data
    The program now reads building data from input.json, processes it into a linked list and a binary search tree, and outputs the state to state.json. It also displays the buildings and the BST order in the console.

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,20 +1,77 @@
 #include <iostream>
 #include <fstream>
 
+#include "json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
 
-int main() {
+/*
+    Se incluyen los otros .cpp
+    porque el proyecto no usa headers.
+*/
+#include "linked_list.cpp"
+#include "tree.cpp"
 
-    ofstream state("state.json");
+int main()
+{
+    ifstream input("../data/input.json");
 
-    state << "{\n";
-    state << "   \"game\":\"City Builder\",\n";
-    state << "   \"status\":\"running\"\n";
-    state << "}\n";
+    if(!input.is_open())
+    {
+        cout << "Error abriendo input.json" << endl;
+        return 1;
+    }
+
+    json data;
+    input >> data;
+
+    LinkedList adjacencyList;
+    BST happinessTree;
+
+    for(auto& building : data["buildings"])
+    {
+        string name = building["name"];
+        int happiness = building["happiness"];
+
+        adjacencyList.insert(name);
+
+        Building b;
+        b.name = name;
+        b.happiness = happiness;
+
+        happinessTree.insert(b);
+    }
+
+    Building happiest = happinessTree.getMax();
+
+    json output;
+
+    output["totalBuildings"] =
+        adjacencyList.size();
+
+    output["happiestBuilding"]["name"] =
+        happiest.name;
+
+    output["happiestBuilding"]["happiness"] =
+        happiest.happiness;
+
+    ofstream state("../data/state.json");
+
+    state << output.dump(4);
 
     state.close();
 
-    cout << "state.json generado correctamente" << endl;
+    cout << "===== BUILDINGS =====" << endl;
+    adjacencyList.display();
+
+    cout << endl;
+    cout << "===== BST ORDER =====" << endl;
+    happinessTree.print();
+
+    cout << endl;
+    cout << "state.json generado correctamente"
+         << endl;
 
     return 0;
 }
